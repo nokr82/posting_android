223959b71c7e2d7848f92bc8957bfd3993cfc716
MinJeung <zarg43@devstories.com>
2018-10-15 14:19:27 +0900

    12

---
+++ app/src/main/java/posting/devstories/com/posting_android/MainActivity.kt
@@ -0,0 +1,13 @@
+package posting.devstories.com.posting_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
