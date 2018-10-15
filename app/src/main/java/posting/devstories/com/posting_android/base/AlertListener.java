116b69479dca0262fbaaea32bb7c66d802a7dfed
MinJeung <zarg43@devstories.com>
2018-10-15 16:30:58 +0900

    12

---
+++ app/src/main/java/posting/devstories/com/posting_android/base/AlertListener.java
@@ -0,0 +1,7 @@
+package posting.devstories.com.posting_android.base;
+
+public interface AlertListener {
+
+    public boolean before();
+    public void after();
+}
---
+++ app/src/main/java/posting/devstories/com/posting_android/base/Config.java
@@ -0,0 +1,10 @@
+package posting.devstories.com.posting_android.base;
+
+public class Config {
+
+    public static String url = "http://13.124.13.37";
+    public static String device = "A";
+    public static String gcm_sender_id = "882000133096";
+    public static String daum_map_api = "c61e7f99808941e6461ebb2a7a49376e"; // 다음 맵 api key real
+
+}
