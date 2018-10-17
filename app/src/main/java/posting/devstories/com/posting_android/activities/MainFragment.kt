54ae803a5e6f05e0592bfe83736d5726a5dd245c
MinJeung <zarg43@devstories.com>
2018-10-17 19:55:16 +0900

    민정 등록

--- app/src/main/java/posting/devstories/com/posting_android/activities/MainFragment.kt
+++ app/src/main/java/posting/devstories/com/posting_android/activities/MainFragment.kt
@@ -1,27 +1,45 @@
 package posting.devstories.com.posting_android.activities
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fra_main.*
+import org.json.JSONObject
 import posting.devstories.com.posting_android.R
+import posting.devstories.com.posting_android.adapter.PostAdapter
 
 open class MainFragment : Fragment() {
 
-    private val BACK_PRESSED_TERM = (1000 * 2).toLong()
-    private var backPressedTime: Long = 0
+    lateinit var activity: MainActivity
+    var tabType = 1;
+
+    var adapterData: ArrayList<JSONObject> = ArrayList<JSONObject>()
+    lateinit var adapter:PostAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val v = inflater.inflate(R.layout.fra_main, container, false)
 
+        activity = getActivity() as MainActivity
+        tabType = activity.tabType
+
+//        adapter = PostAdapter(activity, R.layout.item_post, adapterData)
+//        gideGV.adapter = adapter
+
+
+        loadData();
+
         return v
+    }
+
+    fun loadData(){
     }
 
 }
