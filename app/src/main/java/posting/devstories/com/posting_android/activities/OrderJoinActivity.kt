e1357cb607a2dd42d864b25a44bf7f0223547a3f
youngju <youngjucha0316@gmail.com>
2018-10-17 19:53:07 +0900

    동우 등록

--- app/src/main/java/posting/devstories/com/posting_android/activities/OrderJoinActivity.kt
+++ app/src/main/java/posting/devstories/com/posting_android/activities/OrderJoinActivity.kt
@@ -1,29 +1,123 @@
 package posting.devstories.com.posting_android.activities
 
+import android.content.Context
 import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_orderjoin.*
 import posting.devstories.com.posting_android.R
 import posting.devstories.com.posting_android.base.RootActivity
+import posting.devstories.com.posting_android.base.Utils
 
 class OrderJoinActivity : RootActivity() {
+    lateinit var context: Context
 
 
-
+    var store = arrayOf("삼성", "엘지")
+    var school2= arrayOf("서울대","인하대","경희대","한양대")
+    lateinit var adpater: ArrayAdapter<String>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orderjoin)
 
+        this.context = this
+
+
+
+        adpater = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,store)
+
+        StoreSP.adapter  = adpater
+
+
+
+        adpater = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, school2)
+        SchoolSP.adapter = adpater
+
+
+
+
+
+
+
+    allCK.setOnCheckedChangeListener{
+        compoundButton, b ->
+        if (b==true){
+            serviceCK.isChecked=true
+            soloCK.isChecked=true
+        }else{
+            serviceCK.isChecked=false
+            soloCK.isChecked=false
+        }
+    }
+
+
+
+        
 
 
 
 
         PostingStartIV.setOnClickListener {
+
+            val getOffice:String = Utils.getString(OfficeET)
+            val getPW:String = Utils.getString(pwET)
+            val getPW2:String = Utils.getString(pw2ET)
+            val getStore:String = Utils.getString(StoreET)
+            val getCeo:String = Utils.getString(ceoET)
+            val getPhone:String = Utils.getString(phoneET)
+
+
+
+
+
+
+
+
+            if(getOffice==""||getOffice==null|| getOffice.isEmpty()){
+                Toast.makeText(context, "사업자등록번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if(getPW==""||getPW==null|| getPW.isEmpty()){
+                Toast.makeText(context, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if(getPW!=getPW2){
+                Toast.makeText(context, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(getStore==""||getStore==null|| getStore.isEmpty()){
+                Toast.makeText(context, "상호명을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(getCeo==""||getCeo==null|| getCeo.isEmpty()){
+                Toast.makeText(context, "대표자 성명을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(getPhone==""||getPhone==null|| getPhone.isEmpty()){
+                Toast.makeText(context, "휴대폰번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (allCK.isChecked!=true){
+                Toast.makeText(context, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (serviceCK.isChecked!=true){
+                Toast.makeText(context, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (soloCK.isChecked!=true){
+                Toast.makeText(context, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+
+
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/posting/devstories/com/posting_android/activities/StudentJoinActivity.kt
+++ app/src/main/java/posting/devstories/com/posting_android/activities/StudentJoinActivity.kt
@@ -1,13 +1,17 @@
 package posting.devstories.com.posting_android.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_studentjoin.*
 import posting.devstories.com.posting_android.R
 import posting.devstories.com.posting_android.base.RootActivity
+import posting.devstories.com.posting_android.base.Utils
 
 class StudentJoinActivity : RootActivity() {
+    lateinit var context: Context
 
     var gender = arrayOf("남", "여")
     var years:ArrayList<String> = ArrayList<String>()
@@ -17,9 +21,70 @@ class StudentJoinActivity : RootActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_studentjoin)
 
+        this.context = this
+
+
+
+        allCK.setOnCheckedChangeListener{
+            compoundButton, b ->
+            if (b==true){
+                serviceCK.isChecked=true
+                soloCK.isChecked=true
+            }else{
+                serviceCK.isChecked=false
+                soloCK.isChecked=false
+            }
+        }
+
+
 
 
         joinDoneIV.setOnClickListener {
+            val getid:String = Utils.getString(idET)
+            val getPW:String = Utils.getString(pwET)
+            val getPW2:String = Utils.getString(pw2ET)
+            val getNick:String = Utils.getString(nickET)
+            val getName:String = Utils.getString(nameET)
+
+
+
+
+            if(getid==""||getid==null|| getid.isEmpty()){
+                Toast.makeText(context, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if(getPW==""||getPW==null|| getPW.isEmpty()){
+                Toast.makeText(context, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if(getPW!=getPW2){
+                Toast.makeText(context, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(getNick==""||getNick==null|| getNick.isEmpty()){
+                Toast.makeText(context, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(getName==""||getName==null|| getName.isEmpty()){
+                Toast.makeText(context, "이름을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (allCK.isChecked!=true){
+                Toast.makeText(context, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (serviceCK.isChecked!=true){
+                Toast.makeText(context, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (soloCK.isChecked!=true){
+                Toast.makeText(context, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+
+
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
