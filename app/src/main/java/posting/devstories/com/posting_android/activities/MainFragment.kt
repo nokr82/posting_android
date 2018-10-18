b1ef8066336784c8f22a21212d952509c5107c0c
MinJeung <zarg43@devstories.com>
2018-10-18 10:46:45 +0900

    민정 등록

--- app/src/main/java/posting/devstories/com/posting_android/activities/MainFragment.kt
+++ app/src/main/java/posting/devstories/com/posting_android/activities/MainFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import kotlinx.android.synthetic.main.fra_main.*
 import org.json.JSONObject
 import posting.devstories.com.posting_android.R
@@ -13,33 +14,51 @@ import posting.devstories.com.posting_android.adapter.PostAdapter
 
 open class MainFragment : Fragment() {
 
+    var context = null
+
     lateinit var activity: MainActivity
     var tabType = 1;
 
     var adapterData: ArrayList<JSONObject> = ArrayList<JSONObject>()
     lateinit var adapter:PostAdapter
 
+    lateinit var gideGV: GridView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        context = container!!.context as Nothing?
 
-        val v = inflater.inflate(R.layout.fra_main, container, false)
+        return inflater.inflate(R.layout.fra_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        gideGV = view.findViewById<GridView>(R.id.gideGV)
+
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
         activity = getActivity() as MainActivity
         tabType = activity.tabType
 
-//        adapter = PostAdapter(activity, R.layout.item_post, adapterData)
-//        gideGV.adapter = adapter
-
+        adapter = PostAdapter(activity, R.layout.item_post, adapterData)
+        gideGV.adapter = adapter
 
         loadData();
 
-        return v
     }
 
     fun loadData(){
+
+
+
     }
 
 }
