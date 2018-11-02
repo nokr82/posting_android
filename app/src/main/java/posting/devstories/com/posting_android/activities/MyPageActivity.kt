5fb8a679304340420b47836c36308e98982cf1d4
youngju <youngjucha0316@gmail.com>
2018-11-02 16:15:42 +0900

    동우 등록

--- app/src/main/java/posting/devstories/com/posting_android/activities/MyPageActivity.kt
+++ app/src/main/java/posting/devstories/com/posting_android/activities/MyPageActivity.kt
@@ -5,31 +5,39 @@ import android.app.ProgressDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.FragmentActivity
-import android.util.Log.d
 import android.view.ContextThemeWrapper
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
+import com.nostra13.universalimageloader.core.ImageLoader
 import cz.msebera.android.httpclient.Header
 import kotlinx.android.synthetic.main.activity_mypage.*
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import posting.devstories.com.posting_android.Actions.MemberAction
 import posting.devstories.com.posting_android.R
-import posting.devstories.com.posting_android.R.id.nameTV
+import posting.devstories.com.posting_android.base.Config
 import posting.devstories.com.posting_android.base.PrefUtils
-import posting.devstories.com.posting_android.base.RootActivity
 import posting.devstories.com.posting_android.base.Utils
+import java.io.*
 
 class MyPageActivity : FragmentActivity() {
 
 
+    var thumbnail:Bitmap? = null
+    var gallery:Bitmap? = null
     var nick = ""
     var name  = ""
     var birth = ""
+    private val GALLERY = 1
+    private val CAMERA = 2
     lateinit var context:Context
     private var progressDialog: ProgressDialog? = null
     var autoLogin = false
@@ -48,6 +56,7 @@ class MyPageActivity : FragmentActivity() {
 
         }
         profileTV.setOnClickListener {
+            showPictureDialog()
 
         }
         nickTV.setOnClickListener {
@@ -61,10 +70,7 @@ class MyPageActivity : FragmentActivity() {
 
 
         joinoutTV.setOnClickListener {
-            PrefUtils.setPreference(context, "autoLogin", autoLogin)
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
+            joinoutdlgView()
         }
         alramTV.setOnClickListener {
 
@@ -91,9 +97,10 @@ class MyPageActivity : FragmentActivity() {
 
 
 
-        finishLL.setOnClickListener {
+        finish3LL.setOnClickListener {
             finish()
         }
+
 
     }
     fun dlgView(){
@@ -112,6 +119,36 @@ class MyPageActivity : FragmentActivity() {
         }
 
         builder.show()
+    }
+
+    fun joinoutdlgView(){
+        var mPopupDlg: DialogInterface? = null
+
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.coupon_dlg, null)
+        val couponnoTX = dialogView.findViewById<TextView>(R.id.couponnoTX)
+        val couponyTX = dialogView.findViewById<TextView>(R.id.couponyTX)
+        val couponTV :TextView = dialogView.findViewById<TextView>(R.id.couponTV)
+        couponTV.text = "로그아웃 하시겠습니까?"
+        couponnoTX.text = "NO"
+        couponyTX.text = "YES"
+
+        couponnoTX.setOnClickListener {
+            mPopupDlg!!.cancel()
+        }
+
+        couponyTX.setOnClickListener {
+
+            PrefUtils.setPreference(context, "autoLogin", autoLogin)
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            mPopupDlg!!.cancel()
+
+        }
+
+        mPopupDlg =  builder.setView(dialogView).show()
+
     }
 
     fun redout() {
@@ -187,6 +224,104 @@ class MyPageActivity : FragmentActivity() {
     }
 
 
+    fun edit_profile(){
+        val params = RequestParams()
+        params.put("member_id", PrefUtils.getIntPreference(context, "member_id"))
+        if (gallery==null){
+        }else{
+            print("===캡쳐"+gallery)
+            params.put("upload", ByteArrayInputStream(Utils.getByteArray(gallery)))
+        }
+        if (thumbnail==null){
+        }else{
+            print(thumbnail)
+            params.put("upload", ByteArrayInputStream(Utils.getByteArray(thumbnail)))
+        }
+
+        MemberAction.edit_info(params, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                try {
+                    val result = response!!.getString("result")
+
+
+
+
+                    if ("ok" == result) {
+
+                        Toast.makeText(context, "프로필이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+
+
+                    } else {
+
+                        Toast.makeText(context, "오류가 발생하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONArray?) {
+                super.onSuccess(statusCode, headers, response)
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, responseString: String?) {
+
+                // System.out.println(responseString);
+            }
+
+            private fun error() {
+                Utils.alert(context, "조회중 장애가 발생하였습니다.")
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, responseString: String?, throwable: Throwable) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                // System.out.println(responseString);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, throwable: Throwable, errorResponse: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, throwable: Throwable, errorResponse: JSONArray?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onStart() {
+                // show dialog
+                if (progressDialog != null) {
+
+                    progressDialog!!.show()
+                }
+            }
+
+            override fun onFinish() {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+            }
+        })
+    }
 
     fun loadInfo() {
         val params = RequestParams()
@@ -208,7 +343,11 @@ class MyPageActivity : FragmentActivity() {
                         nick =  Utils.getString(member, "nick_name")
                         name = Utils.getString(member,"name")
                         birth =  Utils.getString(member,"birth")
-
+                        var image_uri = Utils.getString(member, "image_uri")
+                        if (!image_uri.isEmpty() && image_uri != "") {
+                            var image = Config.url + image_uri
+                            ImageLoader.getInstance().displayImage(image,myproIV, Utils.UILoptionsPosting)
+                        }
                         infonameTV.text = name+"/"+birth
                         nameTV.text =nick
 
@@ -263,6 +402,67 @@ class MyPageActivity : FragmentActivity() {
             }
         })
     }
+    fun showPictureDialog() {
+        val pictureDialog = AlertDialog.Builder(this)
+        pictureDialog.setTitle("프로필 이미지 변경")
+        val pictureDialogItems = arrayOf("갤러리에서 가져오기", "카메라로 사진찍기")
+        pictureDialog.setItems(pictureDialogItems
+        ) { dialog, which ->
+            when (which) {
+                0 -> choosePhotoFromGallary()
+                1 -> takePhotoFromCamera()
+            }
+        }
+        pictureDialog.show()
+    }
+
+    fun choosePhotoFromGallary() {
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(galleryIntent, GALLERY)
+
+    }
+
+    fun takePhotoFromCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA)
+
+    }
+
+    public override fun onActivityResult(requestCode:Int, resultCode:Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY)
+        {
+            if (data != null)
+            {
+                val contentURI = data!!.data
+                try
+                {
+                    gallery = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    edit_profile()
+                    myproIV!!.setImageBitmap(gallery)
+
+                }
+                catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "바꾸기실패", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+        else if (requestCode == CAMERA)
+        {
+            thumbnail = data!!.extras!!.get("data") as Bitmap
+            edit_profile()
+            myproIV!!.setImageBitmap(thumbnail)
+        }
+    }
+
+
+
     override fun onDestroy() {
         super.onDestroy()
 
