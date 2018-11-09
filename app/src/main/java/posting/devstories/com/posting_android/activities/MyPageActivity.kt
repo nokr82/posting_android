d3a8899eb6ab17474a478f6b7ae3dec98482a478
MinJeung <zarg43@devstories.com>
2018-11-09 11:49:24 +0900

    민정 등록

--- app/src/main/java/posting/devstories/com/posting_android/activities/MyPageActivity.kt
+++ app/src/main/java/posting/devstories/com/posting_android/activities/MyPageActivity.kt
@@ -500,7 +500,7 @@ class MyPageActivity : FragmentActivity() {
                         school_email_confirmed = Utils.getString(member, "school_email_confirmed")
                         school_confirmed = Utils.getString(member, "school_confirmed")
 
-                        if(school_confirmed == "N") {
+                        if(school_confirmed == "N" && Utils.getString(member, "member_type") == "2") {
                             schoolTV.visibility = View.VISIBLE
                         } else {
                             schoolTV.visibility = View.GONE
